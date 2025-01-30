854a9f1baa8a19221c1b6edbaf9dd9b9bb96b2d2
ahmed-magdy34 <168830955+ahmed-magdy34@users.noreply.github.com>
2025-01-30 11:43:15 +0200

    responsive design&vaildation

--- scripts/login.js
+++ scripts/login.js
@@ -2,22 +2,36 @@
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const button = document.getElementById("submit-btn");
+const passwordError = document.getElementById("password-error");
+const emailError = document.getElementById("email-error");
 //////////////////////////////////////////////////////
 const loginValues = {};
 ///////////////////////////////////////////////////////
+
 button.addEventListener("click", (e) => {
   e.preventDefault();
+
+  let formIsValid = true;
+
   if (!email.value) {
     email.classList.add("error");
-    return;
+    formIsValid = false;
+    emailError.classList.remove("err-hide");
+  } else {
+    email.classList.remove("error");
+    emailError.classList.add("err-hide");
   }
+
   if (!password.value) {
     password.classList.add("error");
-  }
-  if (email.value && password.value) {
+    passwordError.classList.remove("err-hide");
+    formIsValid = false;
+  } else {
     password.classList.remove("error");
-    email.classList.remove("error");
-    //////////////
+    passwordError.classList.add("err-hide");
+  }
+
+  if (formIsValid) {
     loginValues.email = email.value;
     loginValues.password = password.value;
     email.value = "";
--- scripts/register.js
+++ scripts/register.js
@@ -1,51 +1,78 @@
+"use strict";
+
 const firstName = document.getElementById("first-name");
 const lastName = document.getElementById("last-name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const button = document.getElementById("submit-btn");
 ////////////////////////////////////////////////////
+const emailError = document.getElementById("email-error");
+const passwordError = document.getElementById("password-error");
+const firstNameError = document.getElementById("first-name-error");
+const lastNameError = document.getElementById("last-name-error");
+
+/////////////////////////////////////////////////////
 const registerValues = {};
 const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-///////////////////////////////////////////////////
+/////////////////////////////////////////////////////
 button.addEventListener("click", (e) => {
   e.preventDefault();
+
+  let formIsValid = true;
+
   if (!firstName.value) {
     firstName.classList.add("error");
-    return;
+    formIsValid = false;
+    firstNameError.classList.remove("err-hide");
+  } else {
+    firstName.classList.remove("error");
+    firstNameError.classList.add("err-hide");
   }
+
   if (!lastName.value) {
     lastName.classList.add("error");
-    return;
+    lastNameError.classList.remove("err-hide");
+
+    formIsValid = false;
+  } else {
+    lastNameError.classList.add("err-hide");
+
+    lastName.classList.remove("error");
   }
+
   if (!email.value || !emailPattern.test(email.value)) {
     email.classList.add("error");
-    return;
+    emailError.classList.remove("err-hide");
+
+    formIsValid = false;
+  } else {
+    emailError.classList.add("err-hide");
+
+    email.classList.remove("error");
   }
+
   if (!password.value) {
     password.classList.add("error");
-    return;
-  }
-  ///////////////////////////////////
-  if (
-    email.value &&
-    emailPattern.test(email.value) &&
-    lastName.value &&
-    firstName.value
-  ) {
-    ///////////////////
-    firstName.classList.remove("error");
-    lastName.classList.remove("error");
-    email.classList.remove("error");
-    password.classList.remove("error");
-    //////////////////////
+    passwordError.classList.remove("err-hide");
 
+    formIsValid = false;
+  } else {
+    password.classList.remove("error");
+    passwordError.classList.add("err-hide");
+  }
+
+  if (formIsValid) {
     registerValues.firstName = firstName.value;
     registerValues.lastName = lastName.value;
     registerValues.email = email.value;
-    registerValues.password = email.value;
-  }
-  ///////////////////////////////////
+    registerValues.password = password.value;
 
-  console.log(registerValues);
+    firstName.value = "";
+    lastName.value = "";
+    email.value = "";
+    password.value = "";
+
+    console.log(registerValues);
+  }
 });
