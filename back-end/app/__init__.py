73c8db23ae82f63d9395d208a34ac1264e509499
ahmed-magdy34 <168830955+ahmed-magdy34@users.noreply.github.com>
2025-02-08 00:08:32 +0200

    fixed a bug with jwt

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from datetime import timedelta
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -13,6 +14,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=12)
 
     db.init_app(app)
     bcrypt.init_app(app)
